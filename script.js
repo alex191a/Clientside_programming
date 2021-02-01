a4b428e28d771b99c37ecd89371921dca59ba4ec
Alexander <alex191a@gmail.com>
2021-02-01 12:35:30 +0100

    some changes

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let randomNumber = Math.floor(Math.random()*100);
+let randomNumber = Math.floor(Math.random()*100)+1;;
 let guessCount = 0;
 let resetButton;
 
 function cGuess(){
     let userguess = document.getElementById('guessField').value;
     guessCount++
-    document.getElementById('lastGuess').innerHTML = userguess
-    document.getElementById('guessCount').innerHTML = guessCount;
+    document.getElementById('lastGuess').innerHTML = "Last number was: " + userguess
+    document.getElementById('guessCount').innerHTML = "Guess counter : " + guessCount;
     if( userguess == randomNumber){
         document.getElementById('lastResult').innerHTML = 'Winner Winner, reload the page tu try again';
         document.getElementById('hot').innerHTML ='just right';
@@ -14,12 +14,12 @@ function cGuess(){
     else{
         document.getElementById('lastResult').innerHTML ='try again';
         if (userguess > randomNumber){
-            document.getElementById('hot').innerHTML ='too high';
+            document.getElementById('hot').innerHTML ='Number is too high';
         }
         else if (userguess < randomNumber)
         {
-            document.getElementById('hot').innerHTML ='too low';
+            document.getElementById('hot').innerHTML ='Number is too low';
         }
     }
-    }
-    
+}
+document.getElementById('guessSubmit').addEventListener("click", cGuess());
